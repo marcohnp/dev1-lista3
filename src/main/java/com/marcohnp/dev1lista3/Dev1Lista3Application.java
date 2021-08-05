1a1982dc0795fffb3402e1028138e956210100bb
Marco Henrique Nunes Pereira <marcohnp@hotmail.com>
2021-08-04 21:50:21 -0300

    Projeto enviado ao GitHub

---
+++ src/main/java/com/marcohnp/dev1lista3/Dev1Lista3Application.java
@@ -0,0 +1,13 @@
+package com.marcohnp.dev1lista3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev1Lista3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dev1Lista3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcohnp/dev1lista3/Dev1Lista3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcohnp.dev1lista3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev1Lista3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
