004a2e58f2f29ca4b929e9450cdc0f67b6714e54
Marco Henrique Nunes Pereira <marcohnp@hotmail.com>
2021-08-05 13:09:16 -0300

    Adição da anotação @Repository em ConatoRepository

--- src/main/java/com/marcohnp/dev1lista3/repository/ContatoRepository.java
+++ src/main/java/com/marcohnp/dev1lista3/repository/ContatoRepository.java
@@ -2,6 +2,8 @@ package com.marcohnp.dev1lista3.repository;
 
 import com.marcohnp.dev1lista3.entity.ContatoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContatoRepository extends JpaRepository<ContatoEntity, Integer> {
 }
